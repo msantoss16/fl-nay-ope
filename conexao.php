c95409d1f00cb7eeee2691a8d923103076c5b9b2
Matheus Santos <62575387+msantoss16@users.noreply.github.com>
2020-11-25 17:25:10 -0300

    Delete conexao.php

--- conexao.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$conexao=@mysql_connect("localhost","root","") or die ("Falha de conexao".mysql_error());
-	mysql_select_db("ope1", $conexao) or die ("erro no banco ou inexistente".mysql_error());
-?>	
